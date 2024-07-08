b0429cb2f12f9fa9cd79221ebc4e362c195e5d32
Elroi Abergel <elroiabergel@gmail.com>
2024-07-09 02:45:51 +0300

    Finished hashing the passwords both on frontend and backend on front used hash without salt on back used with salt

--- apps/gateway/src/dto/User/create-user.dto.ts
+++ apps/gateway/src/dto/User/create-user.dto.ts
@@ -1,19 +1,25 @@
-import { IsNotEmpty, IsString, MaxLength, IsStrongPassword,Matches, IsStrongPasswordOptions } from "class-validator";
+import {
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  IsStrongPassword,
+  Matches,
+  IsStrongPasswordOptions,
+  IsEmail,
+} from 'class-validator';
 export class CreateUserDto {
+  @IsString()
+  @IsEmail({})
+  @MaxLength(20)
+  @IsNotEmpty()
+  readonly email: string;
+  @IsString()
+  @MaxLength(20)
+  @Matches(/^[a-zA-Z0-9]{2,8}$/)
+  @IsNotEmpty()
+  readonly username: string;
+  @IsNotEmpty()
+   password: string;
 
-    @IsString()
-    @MaxLength(20)
-    @Matches(/^[a-zA-Z0-9]{2,8}$/)
-    @IsNotEmpty()
-    readonly username: string;
-    @IsStrongPassword({
-        minLength: 8,
-        minLowercase: 1,
-        minNumbers: 3,
-        minUppercase: 1,
-        minSymbols:0
-      } as IsStrongPasswordOptions)    
-    @MaxLength(30)
-    @IsNotEmpty()
-    readonly password: string;
+  salt: string;
 }
